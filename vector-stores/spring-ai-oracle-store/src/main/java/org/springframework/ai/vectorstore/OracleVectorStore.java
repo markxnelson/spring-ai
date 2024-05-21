73e6b77e439bddd02c2e31ee3e96d09930c76df8
Mark Nelson <mark.x.nelson@oracle.com>
2024-05-21 13:59:43 -0400

    initial impl
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

---
+++ vector-stores/spring-ai-oracle-store/src/main/java/org/springframework/ai/vectorstore/OracleVectorStore.java
@@ -0,0 +1,316 @@
+/*
+ * Copyright 2023 - 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.vectorstore;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
+
+//Oracle DB
+import oracle.jdbc.OracleType;
+import oracle.sql.json.OracleJsonFactory;
+import oracle.sql.json.OracleJsonObject;
+
+/**
+ * Uses Oracle Database 23ai Vector feature to store Spring AI vectors.
+ *
+ * @author Corrado De Bari
+ * @author Mark Nelson
+ */
+public class OracleVectorStore implements VectorStore, InitializingBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(OracleVectorStore.class);
+
+	public static final int OPENAI_EMBEDDING_DIMENSION = 1536;
+
+	public static final int INVALID_EMBEDDING_DIMENSION = -1;
+
+	public String VECTOR_TABLE = "vector_store";
+
+	public int BATCH_SIZE = 100;
+
+	private JdbcTemplate jdbcTemplate;
+
+	EmbeddingClient embeddingClient;
+
+	private int dimensions;
+
+	private OracleDistanceType distanceType;
+
+	private boolean removeExistingVectorStoreTable;
+
+	public enum OracleIndexType {
+
+		/**
+		 * Performs exact search.
+		 */
+		NONE,
+		/**
+		 * Inverted File Flat (IVF) is a form of Neighbor Partition Vector index. It is a
+		 * partition-based index that achieves search efficiency by narrowing the search
+		 * area through the use of neighbor partitions or clusters.
+		 */
+		IVF,
+		/**
+		 * A Hierarchical Navigable Small World Graph (HNSW) is a form of In-Memory
+		 * Neighbor Graph vector index. It is a very efficient index for vector
+		 * approximate similarity search.
+		 */
+		HNSW;
+
+	}
+
+	public enum OracleDistanceType {
+
+		COSINE("<=>", "COSINE_DISTANCE"), DOT("<#>", "INNER_PRODUCT"), EUCLIDEAN("<->", "L2_DISTANCE"),
+		MANHATTAN(null, "L1_DISTANCE");
+
+		public final String operator;
+
+		public final String shorthandFunction;
+
+		OracleDistanceType(String operator, String shorthandFunction) {
+			this.operator = operator;
+			this.shorthandFunction = shorthandFunction;
+		}
+
+	}
+
+	public OracleVectorStore(JdbcTemplate jdbcTemplate, EmbeddingClient embeddingClient) {
+		this(jdbcTemplate, embeddingClient, INVALID_EMBEDDING_DIMENSION, OracleDistanceType.COSINE, false);
+	}
+
+	public OracleVectorStore(JdbcTemplate jdbcTemplate, EmbeddingClient embeddingClient, int dimensions,
+			OracleDistanceType distanceType, boolean removeExistingVectorStoreTable) {
+		this.jdbcTemplate = jdbcTemplate;
+		this.embeddingClient = embeddingClient;
+		this.dimensions = dimensions;
+		this.distanceType = distanceType;
+		this.removeExistingVectorStoreTable = removeExistingVectorStoreTable;
+	}
+
+	@Override
+	public void add(List<Document> documents) {
+
+		int size = documents.size();
+
+		this.jdbcTemplate.batchUpdate("insert into " + this.VECTOR_TABLE + " (text,embeddings,metadata) values (?,?,?)",
+				new BatchPreparedStatementSetter() {
+					@Override
+					public void setValues(PreparedStatement ps, int i) throws SQLException {
+
+						var document = documents.get(i);
+						var text = document.getContent();
+
+						OracleJsonFactory factory = new OracleJsonFactory();
+						OracleJsonObject jsonObj = factory.createObject();
+						Map<String, Object> metaData = document.getMetadata();
+						for (Map.Entry<String, Object> entry : metaData.entrySet()) {
+							jsonObj.put(entry.getKey(), String.valueOf(entry.getValue()));
+						}
+
+						List<Double> vectorList = embeddingClient.embed(document);
+						double[] embeddings = new double[vectorList.size()];
+						for (int j = 0; j < vectorList.size(); j++) {
+							embeddings[j] = vectorList.get(j);
+						}
+
+						ps.setString(1, text);
+						ps.setObject(2, embeddings, OracleType.VECTOR);
+						ps.setObject(3, jsonObj, OracleType.JSON);
+
+					}
+
+					@Override
+					public int getBatchSize() {
+						return size;
+					}
+				});
+
+	}
+
+	@Override
+	public Optional<Boolean> delete(List<String> idList) {
+
+		String sql = "delete from " + this.VECTOR_TABLE + " where id = ?";
+		int count[][] = jdbcTemplate.batchUpdate(sql, idList, BATCH_SIZE, (ps, argument) -> {
+			ps.setString(1, argument);
+		});
+
+		int sum = Arrays.stream(count).flatMapToInt(Arrays::stream).sum();
+		logger.debug("Deleted " + sum + " records");
+
+		return Optional.of(sum == idList.size());
+	}
+
+	@Override
+	public List<Document> similaritySearch(SearchRequest request) {
+
+		List<VectorData> nearest = new ArrayList<>();
+
+		logger.debug("Requested query " + request.getQuery());
+
+		List<Double> queryEmbeddings = embeddingClient.embed(request.getQuery());
+		logger.debug("Embeddings size: " + queryEmbeddings.size());
+
+		logger.debug("Distance metrics: " + this.distanceType);
+		logger.debug("Distance metrics function: " + this.distanceType.shorthandFunction);
+
+		int topK = request.getTopK();
+
+		try {
+			nearest = similaritySearchByMetrics(VECTOR_TABLE, queryEmbeddings, topK,
+					this.distanceType.shorthandFunction);
+		}
+		catch (Exception e) {
+			logger.error(e.toString());
+		}
+
+		List<Document> documents = new ArrayList<>();
+
+		for (VectorData d : nearest) {
+			OracleJsonObject metadata = d.getMetadata();
+			Map<String, Object> map = new HashMap<>();
+			for (String key : metadata.keySet()) {
+				map.put(key, metadata.get(key).toString());
+			}
+			Document doc = new Document(d.getText(), map);
+			documents.add(doc);
+
+		}
+		return documents;
+
+	}
+
+	List<VectorData> similaritySearchByMetrics(String vectortab, List<Double> vector, int topK,
+			String distance_metrics_func) throws SQLException {
+		List<VectorData> results = new ArrayList<>();
+		double[] doubleVector = new double[vector.size()];
+		for (int i = 0; i < vector.size(); i++) {
+			doubleVector[i] = vector.get(i);
+		}
+
+		try {
+
+			String similaritySql = "SELECT id,embeddings,metadata,text FROM " + vectortab + " ORDER BY "
+					+ distance_metrics_func + "(embeddings, ?)" + " FETCH FIRST ? ROWS ONLY";
+
+			results = jdbcTemplate.query(similaritySql, new PreparedStatementSetter() {
+				public void setValues(java.sql.PreparedStatement ps) throws SQLException {
+					ps.setObject(1, doubleVector, OracleType.VECTOR);
+					ps.setObject(2, topK, OracleType.NUMBER);
+				}
+			}, new RowMapper<VectorData>() {
+				public VectorData mapRow(ResultSet rs, int rowNum) throws SQLException {
+					return new VectorData(rs.getString("id"), rs.getObject("embeddings", double[].class),
+							rs.getObject("text", String.class), rs.getObject("metadata", OracleJsonObject.class));
+				}
+			});
+
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return results;
+	}
+
+	// ---------------------------------------------------------------------------------
+	// Initialize
+	// ---------------------------------------------------------------------------------
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		int initialSize = 0;
+		try {
+
+			initialSize = jdbcTemplate.queryForObject("select count(*) from " + this.VECTOR_TABLE, Integer.class);
+			logger.debug("Table " + this.VECTOR_TABLE + " exists with " + initialSize + " chunks.");
+
+			if (removeExistingVectorStoreTable) {
+				logger.debug("Dropping table " + this.VECTOR_TABLE + " because removeExistingVectorStoreTable = true.");
+				jdbcTemplate.execute(String.format("""
+						begin
+						  drop table %s;
+						exception
+						  when other then
+						    if sqlcode != -942 then
+						      raise;
+						    end if;
+						end;
+						""", this.VECTOR_TABLE));
+			}
+		}
+		catch (Exception e) {
+			try {
+				logger.debug("Dropping table " + this.VECTOR_TABLE);
+				jdbcTemplate.execute(String.format("""
+						        begin
+						          execute immediate 'drop table %s cascade constraints';
+						        exception
+						          when others then
+						            if sqlcode != -942 then
+						              raise;
+						            end if;
+						        end;
+						""", this.VECTOR_TABLE));
+			}
+			catch (Exception ex1) {
+				logger.error("Error dropping table " + this.VECTOR_TABLE + " \n" + e.getMessage());
+				throw (ex1);
+			}
+
+			try {
+				this.jdbcTemplate.execute(String.format("""
+						        begin
+						          execute immediate 'create table %s (
+						            id number generated as identity,
+						            text clob,
+						            embeddings vector,
+						            metadata json,
+						            primary key (id))';
+						        exception
+						          when others then
+						            if sqlcode != -942 then
+						              raise;
+						            end if;
+						        end;
+						""", this.VECTOR_TABLE));
+				logger.debug("Create table " + this.VECTOR_TABLE);
+			}
+			catch (Exception ex2) {
+				logger.error("Error creating table\n" + e.getMessage());
+				throw (ex2);
+			}
+		}
+		return;
+	}
+
+}
---
+++ vector-stores/spring-ai-oracle-store/src/test/java/org/springframework/ai/vectorstore/OracleVectorStoreIT.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2023 - 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.vectorstore;
+
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.openai.OpenAiEmbeddingClient;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.oracle.OracleContainer;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Corrado De Bari
+ * @author Mark Nelson
+ */
+@Testcontainers
+@EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".+")
+public class OracleVectorStoreIT {
+
+	@Container
+	static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-free:23.4-slim-faststart")
+		.withDatabaseName("pdb1")
+		.withUsername("testuser")
+		.withPassword(("testpwd"));
+
+	List<Document> documents = List.of(
+			new Document(getText("classpath:/test/data/spring.ai.txt"), Map.of("meta1", "meta1")),
+			new Document(getText("classpath:/test/data/time.shelter.txt")),
+			new Document(getText("classpath:/test/data/great.depression.txt"), Map.of("meta2", "meta2")));
+
+	public static String getText(String uri) {
+		var resource = new DefaultResourceLoader().getResource(uri);
+		try {
+			return resource.getContentAsString(StandardCharsets.UTF_8);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+		.withUserConfiguration(TestApplication.class)
+		.withPropertyValues("test.spring.ai.vectorstore.pgvector.distanceType=COSINE_DISTANCE",
+
+				// JdbcTemplate configuration
+				String.format("app.datasource.url=%s", oracleContainer.getJdbcUrl()),
+				"app.datasource.username=testuser", "app.datasource.password=testpwd",
+				"app.datasource.type=com.zaxxer.hikari.HikariDataSource");
+
+	private static void dropTable(ApplicationContext context) {
+		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+		jdbcTemplate.execute("drop table vector_store");
+	}
+
+	@ParameterizedTest(name = "{0} : {displayName} ")
+	@ValueSource(strings = { "COSINE", "DOT", "EUCLIDEAN", "MANHATTAN" })
+	public void addAndSearch(String distanceType) {
+		contextRunner.withPropertyValues("test.spring.ai.vectorstore.oracle.distanceType=" + distanceType)
+			.run(context -> {
+
+				VectorStore vectorStore = context.getBean(VectorStore.class);
+
+				vectorStore.add(documents);
+
+				List<Document> results = vectorStore
+					.similaritySearch(SearchRequest.query("What is Great Depression").withTopK(1));
+
+				assertThat(results).hasSize(1);
+				Document resultDoc = results.get(0);
+				assertThat(resultDoc.getId()).isEqualTo(documents.get(2).getId());
+				assertThat(resultDoc.getMetadata()).containsKeys("meta2", "distance");
+
+				// Remove all documents from the store
+				vectorStore.delete(documents.stream().map(doc -> doc.getId()).toList());
+
+				List<Document> results2 = vectorStore
+					.similaritySearch(SearchRequest.query("Great Depression").withTopK(1));
+				assertThat(results2).hasSize(0);
+
+				dropTable(context);
+			});
+	}
+
+	@SpringBootConfiguration
+	@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+	public static class TestApplication {
+
+		@Value("${test.spring.ai.vectorstore.oracle.distanceType}")
+		OracleVectorStore.OracleDistanceType distanceType;
+
+		@Bean
+		public VectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingClient embeddingClient) {
+			return new OracleVectorStore(jdbcTemplate, embeddingClient, OracleVectorStore.INVALID_EMBEDDING_DIMENSION,
+					distanceType, true);
+		}
+
+		@Bean
+		public JdbcTemplate myJdbcTemplate(DataSource dataSource) {
+			return new JdbcTemplate(dataSource);
+		}
+
+		@Bean
+		@Primary
+		@ConfigurationProperties("app.datasource")
+		public DataSourceProperties dataSourceProperties() {
+			return new DataSourceProperties();
+		}
+
+		@Bean
+		public HikariDataSource dataSource(DataSourceProperties dataSourceProperties) {
+			return dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+		}
+
+		@Bean
+		public EmbeddingClient embeddingClient() {
+			return new OpenAiEmbeddingClient(new OpenAiApi(System.getenv("OPENAI_API_KEY")));
+		}
+
+	}
+
+}
