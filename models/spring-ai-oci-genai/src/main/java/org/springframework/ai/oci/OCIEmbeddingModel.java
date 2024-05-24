7bb15dd72d23de641d850d792db6e411afee89ed
Anders Swanson <anders.swanson@oracle.com>
2024-05-24 14:21:25 -0700

    Init OCI GenAI Embedding Model
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

---
+++ models/spring-ai-oci-genai/src/main/java/org/springframework/ai/oci/OCIEmbeddingModel.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.oci;
+
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.AbstractEmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingRequest;
+import org.springframework.ai.embedding.EmbeddingResponse;
+
+public class OCIEmbeddingModel extends AbstractEmbeddingModel {
+    @Override
+    public EmbeddingResponse call(EmbeddingRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<Double> embed(Document document) {
+        return List.of();
+    }
+}
